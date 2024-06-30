0bef292d0e83f2f1977e7c513eff026b2950de6c
Rishabh Rathore <rishabhrathore@Rishabhs-MacBook-Pro.local>
2024-06-30 20:30:52 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function Navbar() {
+
+export default function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
     <div className="container-fluid">
-      <a className="navbar-brand" href="/">AnnaConnect</a>
+      <a className="navbar-brand" href="/">{props.title}</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -31,3 +33,11 @@ export default function Navbar() {
   </nav>
   )
 }
+
+Navbar.prototype = {
+    title : PropTypes.string.isRequired
+};
+
+Navbar.defaultProps = {
+    title : "Send title Prop"
+};
