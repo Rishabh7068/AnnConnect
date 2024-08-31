330137e4539ab3ebdfde52fbc42b82464b5dc866
Pragati Tiwari <tiwaripragati962@gmail.com>
2024-08-31 22:49:25 +0530

    added styling

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,21 +1,18 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
 export default function Footer() {
   return (
-    <div>
-      <div className='footer'>
-        <div className='col-6'>
-          <h3>AnnaConnect</h3>
-          <h5>Email : annaconnect@support.org</h5>
-          <h5>Contact No. : xxx</h5>
-          <h5>AnnaConnect All Rights Reserved</h5>
-        </div>
-        <div className='col-6'>
-          <div>Our Offices: csfess</div>
-          <div >Licence Number: nndnls</div>
-        </div>
+    <footer class="footer">
+      <div class="footer-content">
+        <p class="footer-title">AnnaConnect</p>
+        <p>
+          Email:{" "}
+          <a href="mailto:annaconnect@support.org">annaconnect@support.org</a>
+        </p>
+        <p>Contact No.: xxx-xxx-xxxx</p>
+        <p>&copy; 2024 AnnaConnect. All Rights Reserved.</p>
       </div>
-    </div>
-  )
+    </footer>
+  );
 }
